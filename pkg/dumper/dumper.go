7b51bc8d3f4ed092a8ab6ebf284cd90f39813b69
Debdut <debdutdeb@outlook.com>
2026-09-08 00:32:42 +0530

    docs: update `Dumper` docs

--- pkg/dumper/dumper.go
+++ pkg/dumper/dumper.go
@@ -5,10 +5,14 @@ import (
 	"io"
 )
 
-// Dumper is any object that dumps some data from some source that depends on the Dumper implementation.
+// Dumper produces a stream of data from a source determined by the implementation.
 type Dumper interface {
-	// Dump writes some data from some source into an [io.ReadCloser] object.
-	// Caller must call Close on the returned ReadCloser. An initial Dump attempt may pass, only way 
-	// subsequent errors are propagated are through ReadCloser's Read and Close receivers.
+	// Dump starts producing the data.
+    //
+    // An error returned directly indicates that producing the stream could not
+    // be started. Errors encountered after Dump returns are reported through
+    // the returned [io.ReadCloser].
+    //
+    // The caller must close the returned ReadCloser.
 	Dump(ctx context.Context) (io.ReadCloser, error)
 }
